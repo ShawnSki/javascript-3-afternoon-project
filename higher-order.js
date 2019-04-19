d9f44207d1a8f366ac4d8ca3e5b58b1e59ff73f8
shawn's macbook <shawnscivally@shawn-ski.local>
2019-04-19 15:01:25 -0600

    completed probem 2

--- higher-order.js
+++ higher-order.js
@@ -20,9 +20,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
+let evenNumbers = mixedNumbers.filter(function(element, index, wholeArray){
+    return element % 2 === 0
+  })
 
 
 ////////// PROBLEM 2 //////////
@@ -44,8 +45,13 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// let postTaxPrices = prices.map(function(element, index, wholeArray) {
+//     return element * 1.07
+// })
 
+let postTaxPrices = prices.map((element, index, wholeArray) => {
+  return element * 1.07
+})
 
 
 ////////// PROBLEM 3 //////////
